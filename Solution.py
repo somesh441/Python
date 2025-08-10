d5a49ff990d4a9c1be60296860d7494eb230eafa
somesh441 <119757318+somesh441@users.noreply.github.com>
2025-08-10 15:40:53 +0530

    Create Solution.py

---
+++ Solution.py
@@ -0,0 +1,19 @@
+def length_of_longest_substring(s):
+    char_set = set()
+    left = 0  
+    max_length = 0
+
+    for right in range(len(s)):
+       
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left += 1
+        char_set.add(s[right])
+        max_length = max(max_length, right - left + 1)
+
+    return max_length
+
+
+#Example
+s = "abcabcbb"
+print("Length of longest substring:", length_of_longest_substring(s))
